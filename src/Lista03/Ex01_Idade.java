c40c57bcc3b61f83247fa29a377a87395ac0ff6e
MayconW <83735465+MayconW@users.noreply.github.com>
2023-08-07 16:44:40 -0300

    Pratica com Java para Nivelamento Solutis

---
+++ src/Lista03/Ex01_Idade.java
@@ -0,0 +1,10 @@
+package Lista03;
+
+public class Ex01_Idade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Lista04/Main.java
+++ src/Lista04/Main.java
@@ -1,19 +1,22 @@
 package Lista04;
 
+
 public class Main {
 	
 	public static void linhas() {
 		
-		System.out.println("---------- \n ");
+		System.out.println("\n---------- \n ");
 	}
 	public static void main(String[] args) {
 		
-		
+
+
+
 		Cachorro Max = new Cachorro("Max", 1);
 		Peixe Nemo = new Peixe("Nemo", 1, 4, 2.3);
 		Leao Simba = new Leao("Simba", 5 );
 		
-		Pombo Pru = new Pombo ("Pruu", 1, 2, 0.3, 0.5);
+		Pombo Pru = new Pombo ("Pru", 1, 2, 0.3, 0.5);
 		
 		
 		linhas();
