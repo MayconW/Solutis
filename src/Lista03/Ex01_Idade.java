4bc9bea2c4ec4ffc63da34e1e3afd148ae3e66ef
MayconW <83735465+MayconW@users.noreply.github.com>
2023-08-07 17:16:04 -0300

    Ex 1

--- src/Lista03/Ex01_Idade.java
+++ src/Lista03/Ex01_Idade.java
@@ -1,10 +1,26 @@
 package Lista03;
 
+import java.util.Scanner;
+
 public class Ex01_Idade {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    public static void main(String[] args) {
+
+        int anoNascimento, anoAtual;
+
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Digite o ano de nascimento: ");
+        anoNascimento = s.nextInt();
+
+        System.out.print("Digite o ano atual: ");
+        anoAtual = s.nextInt();
+
+        s.close();
+        System.out.print("Idade: " + (anoAtual - anoNascimento));
+
+    }
 
 }
